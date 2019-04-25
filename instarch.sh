c5dbadf5be4226a38a66358a64696c7228ac1f8b
Paul R <42287355+ftlol@users.noreply.github.com>
2019-04-25 13:43:00 -0500

    Create instarch.sh

---
+++ instarch.sh
@@ -0,0 +1,38 @@
+pacman -Sy
+pacman -S btrfs-progs dosfstools mtools
+nano /etc/pacman.d/mirrorlist # edit mirrors
+cfdisk /dev/sda
+#make three partiotions, 512M, 25G , 8G
+mkfs.fat -F32 /dev/sda1
+mkfs.btrfs /dev/sda2
+mkswap /dev/sda3
+swapon /dev/sda3
+mount /dev/sda2 /mnt
+pacstrap -i /mnt base base-devel
+genfstab -U -p /mnt >> /mnt/etc/fstab
+arch-chroot /mnt /bin/bash
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+nano /etc/locale.gen
+#edit out en_US.UTF-8 UTF-8 then save it
+locale-gen
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+hwclock --systohc
+echo pc > /etc/hostname
+nano /etc/hosts
+#127.0.0.1	localhost
+#::1		localhost
+#127.0.1.1       localhost.localdomain pc
+#save and exit nano
+pacman -S networkmamager
+systemctl enable NetworkManager
+passwd
+#type password twice
+
+
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+unmount -R /mnt
+reboot
