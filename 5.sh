72254b2a810067e13d2a913a3eafb3cfec2d14cb
Paul R <42287355+ftlol@users.noreply.github.com>
2019-04-26 10:43:12 -0500

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,8 +1,1 @@
-echo pc > /etc/hostname
-nano /etc/hosts
-#127.0.0.1	localhost
-#::1		localhost
-#127.0.1.1       localhost.localdomain pc
-#save and exit nano
-pacman -S networkmamager
-systemctl enable NetworkManager
+passwd #enter root password twice
