6640e136948dc47cd1f2d5df1ab990402ef23379
Paul R <42287355+ftlol@users.noreply.github.com>
2019-04-26 10:42:23 -0500

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -6,3 +6,5 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "pc" > /etc/hostname
 echo "127.0.0.1 localhost pc" > /etc/hosts
 echo "::1 localhost pc" >> /etc/hosts
+pacman -S networkmamager
+systemctl enable NetworkManager
