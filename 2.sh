d07927c26c87b9df8f8fc9e9a2ae0878a821590e
Paul R <42287355+ftlol@users.noreply.github.com>
2019-04-26 10:35:07 -0500

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,7 +1,9 @@
 cfdisk /dev/sda
 #make three partiotions, 512M, 25G , 8G
-mkfs.fat -F32 /dev/sda1
-mkfs.btrfs /dev/sda2
+mkfs.fat -F 32 -n EFI /dev/sda1
+mkfs.btrfs -L root /dev/sda2
 mkswap /dev/sda3
 swapon /dev/sda3
 mount /dev/sda2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
