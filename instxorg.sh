2ff594549724c11262d492a78cc99e537d6fa406
Paul R <42287355+ftlol@users.noreply.github.com>
2019-05-06 17:38:34 +0000

    Delete instxorg.sh

--- instxorg.sh
+++
@@ -1,6 +0,0 @@
-pacman -S xorg xorg-xinit pulseaudio xorg-server xterm xorg-xclock xfce4 xfce4-goodies nvidia nvidia-utils nvidia-settings btrfs-progs dosfstools mtools
-#echo "exec startxfce" > ~/.xinitrc
-#sudo X -configure
-#sudo nvidia-xconfig
-#startx = xorg
-#startxfce4 = xfce4 desktop
