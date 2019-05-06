7ac778504d3d39a668aa470825b11aed3c4c55cf
Paul R <42287355+ftlol@users.noreply.github.com>
2019-05-06 17:38:29 +0000

    Update instxorg.sh

--- instxorg.sh
+++ instxorg.sh
@@ -1,4 +1,4 @@
-pacman -S xorg xorg-xinit pulseaudio pulseaudio-alsa xorg-server xterm xorg-xclock xfce4 xfce4-goodies nvidia nvidia-utils nvidia-settings btrfs-progs dosfstools mtools
+pacman -S xorg xorg-xinit pulseaudio xorg-server xterm xorg-xclock xfce4 xfce4-goodies nvidia nvidia-utils nvidia-settings btrfs-progs dosfstools mtools
 #echo "exec startxfce" > ~/.xinitrc
 #sudo X -configure
 #sudo nvidia-xconfig
